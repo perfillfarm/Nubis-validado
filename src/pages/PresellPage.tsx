89f435e1ae92989ffc86031e7a69608d8b3d245d
perfillfarm <facefarmperfil02@gmail.com>
2025-12-06 14:17:02 -0300

    Updated PresellPage.tsx

--- src/pages/PresellPage.tsx
+++ src/pages/PresellPage.tsx
@@ -40,6 +40,25 @@ export default function PresellPage() {
   useEffect(() => {
     initGooglePixel();
     window.scrollTo(0, 0);
+
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17759690922';
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-17759690922');
+    `;
+    document.head.appendChild(script2);
+
+    return () => {
+      document.head.removeChild(script1);
+      document.head.removeChild(script2);
+    };
   }, []);
 
   useEffect(() => {
